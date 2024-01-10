30f90fef9cc7c006f616069bba83f9c738748d85
Ujwal Mamidi <uji.mamidi@gmail.com>
2024-01-10 21:17:22 +1100

    intial project commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+function Navbar() {
+  return (
+    <div>
+			<p className="text-sm">This is Navbar</p>
+    </div>
+  )
+}
+
+export default Navbar;
---
+++ src/pages/Landing.js
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar"
+
+function Landing() {
+  return (
+    <div className="flex flex-col justify-center align-center text-center">
+      <Navbar />
+      <h1>Landing Page</h1>
+    </div>
+ )
+}
+
+export default Landing;
